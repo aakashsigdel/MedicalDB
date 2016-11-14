4cec8944d7b5718fc49ac0854488110389c90084
Aakash Sigdel <aakash.sigdel@gmail.com>
2016-11-14 06:15:13 +0545

    add crashlytics

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { Provider } from 'react-redux'
 import store from './store'
 import Routes from './Routes'
 
-const App = () => <Provider store={store}>
-    <Routes />
-</Provider>
+const App = () =>
+    <Provider store={store}>
+        <Routes />
+    </Provider>
 
 export default App
